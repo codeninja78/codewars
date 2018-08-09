039f012e93eb9c64041ef0f79a1f4f7751851486
Rokko <i.am.rokko@gmail.com>
2018-08-09 13:02:47 +0000

    Add thought process behind Kata to format_a_string_of_names_like_bart

--- Ruby/format_a_string_of_names_like_bart.rb
+++ Ruby/format_a_string_of_names_like_bart.rb
@@ -20,6 +20,19 @@ list([])
 # returns ''
 Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
 
+
+Thought Process:
+For this we will use the .map method so that we can access the objects
+
+We will use x.values to get the values of the name keys
+
+They want us to return with a comma after each name 
+.join(", ")
+
+
+Until the last name where we will use '&'
+.gsub(/(.*)(,)(.*)/, '\1 &\3')
+
 =end
 
 #Solution:
