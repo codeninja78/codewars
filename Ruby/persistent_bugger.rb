f0eb9de65ad78a5a25e8c94504450892a97a4259
Rokko <i.am.rokko@gmail.com>
2018-08-27 12:50:51 +0000

    Edit persistent_bugger to include thought process for solution

--- Ruby/persistent_bugger.rb
+++ Ruby/persistent_bugger.rb
@@ -13,6 +13,27 @@ For example:
 
  persistence(4) # returns 0, because 4 is already a one-digit number
  
+
+Thought Process:
+I've done a similar problem where instead of multiplying the digit I had to sum the digit.
+
+Using the same logic first we will check if the number is less than 10 
+    n < 10 
+
+If the number is less than 10 we will return 0 like the examples have asked us to do
+    ? 0
+    
+Else we will use recursion to loop until we receive a single digit.
+
+We will do this by first changing the number parameter into a string so that we can manipulate each digit of the integer
+    persistence(n.to_s.split(''))
+
+Next we will use the .map method to iterate through the length of the split digits as integers
+    .map(&:to_i)
+    
+Finally this we will us .reduce so that we can multiply those digits
+    .reduce(:*)
+
 =end
 
 #Solution:
